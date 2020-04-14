2d5da2407e371f582d9172d275b70fa0ce89e303
Akuyala <50940726+Akuyala@users.noreply.github.com>
2020-04-14 22:29:56 +1200

    Add files via upload
    Adding the route table and some operations around it

--- demon.py
+++ demon.py
@@ -3,6 +3,7 @@
     RIP routing demon
 
     Kayle Ransby (34043590)
+    Sjaak Flick (36406121)
 
 """
 
@@ -21,13 +22,14 @@ class router():
     output_ports = []
 
     input_sockets = []
+    route_table = None
+    trash_panda_collector = 0
 
     def __init__(self, config):
         """
         Function to read the config file
         """
-
-        # Open configuration file:
+            
         file = open(config)
 
         # Set router ID:
@@ -63,9 +65,18 @@ class router():
     
 
     def port_check(self, port):
-        """
-        """
+        "Returns an error if the port is out of the allowed range"
         return 0 if port >= 1024 or port <= 64000 else self.error("Port outside range 1024 <= x <= 64000")
+
+
+    def metric_check(self, metric):
+        "returns false if the metric is not within the bounds, will also trigger a dead-timer."
+        return True if metric > 0 and metric < 16 else False
+    
+    
+    def rip_version_check(self, value):
+        "Returns false if the rip version != 2, might indicate transmission error"
+        return True if value == 2 else False
 
 
     def error(self, message, exit_code = 1):
@@ -103,12 +114,72 @@ class router():
     def loop(self):
         """
         """
-
+        raise NotImplementedError("This isn't working yet, pls come back later")
+    
+    
+    
+    def rip_packet_header(self):
+        "what every packet needs for its header"
+        packet_to_send = bytearray()
+        command = int(bin(0).zfill(8),1) 
+        packet_to_send += (command).to_bytes(1, byteorder="big")
+        version_num = int(bin(2).zfill(8),1)
+        packet_to_send += (version_num).to_bytes(1, byteorder="big")
+        THIS_HAS_TO_BE_ZERO = int(bin(0).zfill(16),2)
+        packet_to_send += (THIS_HAS_TO_BE_ZERO).to_bytes(2, byteorder="big")
+        
+        
+    def send_packet(self):
+        "sending the packet to the other 'routers'"
+        raise NotImplementedError
+        
+        
+    def create_route_table(self, router_ids, input_ports, output_ports):
+        "creating the route table"
+        route_table = {}
+        for port in output_ports:
+            port = port.split('-')
+            value = (port[2], port[0])
+            route_table[value] = port[1]
+        self.route_table = route_table
+    
+    
+    def add_route_to_table(self, route, metric):
+        "adding a route to the route table. The key needs to be (router_id, portnum)"
+        route_table[route] += metric
+    
+    
+    def modify_route_in_table(self, route, n=1):
+        "modifying a route in the table"
+        route_table[route] += n
+    
+    
+    def delete_route_in_table(self, route):
+        "deleting a route in the table"
+        try:
+            route_table.delete(route)
+        except:
+            return self.error("Route is not in the table")
+    
+    def print_route_table(self):
+        for key, metric in route_table.items():
+            print("Router ID: {} | Port Number: {} | Metric: {}".format(key[0], key[1], metric))
+    
+    
+    def process_packet(self, packet):
+        #prehaps "decode" the packet
+        #then do error checking on each of the fixed fields, 
+        #
+        good_value = metric_check(value)
+        if good_value is False:
+            self.error("Route is gone")
+        
+    
 def main(param):
     """
     Main exicution function
     """
-
+    
     config = param[1]
 
     if len(param) != 2:
@@ -120,4 +191,3 @@ def main(param):
 
 if __name__ == "__main__":
     param = sys.argv
-    main(param)
